5f374cf4e46bc69cfdfe63993c9ead9179102b86
patryck17HUB <ppoumian17@alumnos.uaq.mx>
2024-08-24 13:50:13 -0600

    Pre-jwt

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+//Settings
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Routes
+const banco = require('./routes/banco');
+const users = require('./routes/users');
+
+//Middlewares
+//const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const index = require('./middleware/index');
+//const cors = require('./middleware/cors');
+
+app.get('/', index);
+
+app.use("/banco", banco);
+app.use("/users", users);
+
+//app.use(auth);
+
+app.use(notFound);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running on port 3000');
+});
